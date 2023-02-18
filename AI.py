59290d80bfa4c7f0ce69cd286a6be3a4ed833331
ParkKeunWoo <87476531+pkw0411@users.noreply.github.com>
2023-02-18 22:09:37 +0900

    [update] AI 변수 수정

--- AI.py
+++ AI.py
@@ -9,44 +9,44 @@ import pickle
 current_path = os.path.dirname(__file__)
 
 # p2에 넣어줄 게임 플레이 방식
-b = []
-c = []
+Action_list = []
+Fixed_Action_list = []
 
 # 파일이 있으면 실행
 if os.path.isfile(current_path+'\\play_record.csv'):
     # 데이터 가져오기
-    df = pd.read_csv(current_path+'\\play_record.csv')
+    DataFrame = pd.read_csv(current_path+'\\play_record.csv')
 
     #fps만 뽑아 비교
-    d = df['fps']
-    d_list = list(d.values)
-    max_d = max(d_list)
+    DF_fps = DataFrame['fps']
+    DF_fps_list = list(DF_fps.values)
+    max_DF_fps = max(DF_fps_list)
     
     # fps 1부터 가장 큰 프레임까지 봅기
-    for i in range(1, max_d+1):
+    for i in range(1, max_DF_fps+1):
 
         # 해당 프레임의 값들 가져오기
-        g = df[df.fps==i]
+        DF_SelectedByFrame = DataFrame[DataFrame.fps==i]
 
         # 프레임값들을 리스트로 변환
-        g_values = list(g.values)
+        DFSF_values = list(DF_SelectedByFrame.values)
         
         # 같은 프레임안 플레이들중 렌덤으로 하나 뽑기
-        a = randint(0,len(g_values)-1)
+        Rnd_Num = randint(0,len(DFSF_values)-1)
         # 렌덤으로 뽑은  플레이 저장
-        b.append(list(g_values[a]))
+        Action_list.append(list(DFSF_values[Rnd_Num]))
     
     # 플레이 정보중 이긴 플레이만 따로 저장
-    for i in b:
+    for i in Action_list:
         
-        b= i[2]
-        c.append(eval(b))
+        Action_list= i[2]
+        Fixed_Action_list.append(eval(Action_list))
     
-    print(str(c))
+    print(str(Fixed_Action_list))
 
     #피클에 옮기기
     with open(current_path+'\\play_record.txt','wb') as fw:
-        pickle.dump(c,fw)
+        pickle.dump(Fixed_Action_list,fw)
 
 # 파일이 없을때
 else : 
